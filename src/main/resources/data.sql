45b9639fcf4de9f0cf6b758496b6b69da59b344e
titotegard <titotegard@gmail.com>
2023-04-14 14:27:35 +0700

    add data sql
    inject data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,89 @@
+INSERT INTO user_atc
+VALUES( DATE '1996-02-16', true, CURRENT_TIMESTAMP, NULL, 999, CURRENT_TIMESTAMP, '0000000000000001', 'system', 'Darmawan', 'Tito', 'Tegar', 'system');
+
+    INSERT INTO user_setting
+    VALUES( 999, 999, 'biometric_login', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1000, 999, 'push_notification', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1001, 999, 'sms_notification', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1002, 999, 'show_onboard', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1003, 999, 'widget_order', '1,2,3,4,5');
+
+INSERT INTO user_atc
+VALUES( DATE '1997-02-16', true, CURRENT_TIMESTAMP, NULL, 1000, CURRENT_TIMESTAMP, '0000000000000002', 'system', 'Murdough', 'Alex', NULL, 'system');
+
+    INSERT INTO user_setting
+    VALUES( 1004, 1000, 'biometric_login', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1005, 1000, 'push_notification', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1006, 1000, 'sms_notification', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1007, 1000, 'show_onboard', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1008, 1000, 'widget_order', '1,2,3,4,5');
+
+INSERT INTO user_atc
+VALUES( DATE '1998-02-16', true, CURRENT_TIMESTAMP, NULL, 1001, CURRENT_TIMESTAMP, '0000000000000003', 'system', 'Phelps', 'Michael', 'Ismail', 'system');
+
+    INSERT INTO user_setting
+    VALUES( 1009, 1001, 'biometric_login', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1010, 1001, 'push_notification', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1011, 1001, 'sms_notification', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1012, 1001, 'show_onboard', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1013, 1001, 'widget_order', '1,2,3,4,5');
+
+INSERT INTO user_atc
+VALUES( DATE '1999-02-16', true, CURRENT_TIMESTAMP, NULL, 1002, CURRENT_TIMESTAMP, '0000000000000004', 'system', 'Kujo', 'Jotaro', NULL, 'system');
+
+    INSERT INTO user_setting
+    VALUES( 1014, 1002, 'biometric_login', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1015, 1002, 'push_notification', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1016, 1002, 'sms_notification', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1017, 1002, 'show_onboard', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1018, 1002, 'widget_order', '1,2,3,4,5');
+
+INSERT INTO user_atc
+VALUES( DATE '2000-02-16', true, CURRENT_TIMESTAMP, NULL, 1003, CURRENT_TIMESTAMP, '0000000000000005', 'system', 'Sumbul', 'Ahmad', NULL, 'system');
+
+    INSERT INTO user_setting
+    VALUES( 1019, 1003, 'biometric_login', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1020, 1003, 'push_notification', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1021, 1003, 'sms_notification', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1022, 1003, 'show_onboard', 'false');
+
+    INSERT INTO user_setting
+    VALUES( 1023, 1003, 'widget_order', '1,2,3,4,5');
--- src/main/resources/dataaaa.sql
+++
@@ -1,2 +0,0 @@
--- INSERT INTO user_atc
--- VALUES(1, '1234567890123456', 'Tito', 'Tegar', 'Darmawan', '1996-02-16', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'tito.darmawan', 'tito.darmawan', true, NULL);
